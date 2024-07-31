3444c2cbab6874c01349143052d280f54f62c970
Jean <jeanmacario048@gmail.com>
2024-07-31 17:28:10 -0600

    WO\ Update create Room Type table

--- app/Http/Controllers/Admin/Rooms/RoomTypeController.php
+++ app/Http/Controllers/Admin/Rooms/RoomTypeController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Admin\Rooms;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Rooms\RoomType;
 use Illuminate\Http\Request;
 
 class RoomTypeController extends Controller
 {
     public function index()
     {
-        return inertia('Frontend/Admin/Rooms/CreateRoomType');
+        $roomTypes = RoomType::all();
+        return inertia('Frontend/Admin/Rooms/ShowRoomTypes',
+            ['roomTypes' => $roomTypes]
+        );
     }
 }
